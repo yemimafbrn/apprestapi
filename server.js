14ee9ae67339d71a2dcd39a6414e696f8d08406b
yemima febriana <yemimafbrn@gmail.com>
2020-12-22 00:43:27 +0700

    setting router di serverjs

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+var routes = require(',/routes');
+routes(app);
+
  app.listen(3000, () => {
      console.log('Server started on port');
  });
