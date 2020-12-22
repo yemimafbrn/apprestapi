a131f3ef4335ed9ed4509fb7e9c96029f43560ec
yemima febriana <yemimafbrn@gmail.com>
2020-12-22 10:55:09 +0700

    berhasil menampilkan semua data mahasiswa

--- routes.js
+++ routes.js
@@ -1,11 +1,16 @@
 'use strict';
 
+const { json } = require('body-parser');
+
 module.exports = function(app){
     var jsonku = require('./controller');
 
     app.route('/')
         .get(jsonku.index);
 
-     app.route('(./tampil')
-        .get(jsonku.tampilsemuamahasiswa)
+     app.route('/tampil')
+        .get(jsonku.tampilsemuamahasiswa);
+
+    app.route('/tampil/:id')
+        .get(jsonku.tampilberdasarkanid);
 }
